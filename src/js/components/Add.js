2e98ae43206366eded23d2e3fcead52911c5eda8
Atataika <egorbi2406@gmail.com>
2018-06-26 21:49:00 +0300

    Task 4 light 2

--- src/js/components/Add.js
+++ src/js/components/Add.js
@@ -1,13 +1,13 @@
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
+import { render } from "react-dom";
 import React, { Component } from 'react'
-import { Route, Redirect, Link, browserHistory } from 'react-router'
-import Admin from './Admin'
 
 export default class Add extends Component {
   handleSubmit(e) {
     e.preventDefault()
     const value = e.target.elements[0].value
     window.localStorage.setItem('myKey'+value, '{"title":"'+value+'"}')
-    this.context.router.push('/admin')
 }
 render() {
 	return (
@@ -18,7 +18,7 @@ render() {
 			<div className='row'>
 				<form className='col-md-4' onSubmit={this.handleSubmit}>
 					<input type='text' className='form-control' />
-					<button type='submit' className='btn btn-outline-primary'>Добавить</button>
+					<button type='submit' className='btn btn-outline-primary'><Link to="/admin">Добавить</Link></button>
 				</form>
 			</div>
 		</div>
